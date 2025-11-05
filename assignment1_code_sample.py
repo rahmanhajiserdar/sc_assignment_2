b01c1b41abe14d3d112d60e396c4fdf13c5dc01b
rhajiserdar <rhajiserdar@academic.rrc.ca>
2025-11-05 10:45:37 -0600

    Working on pylinter.

--- assignment1_code_sample.py
+++ assignment1_code_sample.py
@@ -6,21 +6,25 @@ db_config = {"host": "mydatabase.com", "user": "admin", "password": "secret123"}
 
 
 def get_user_input():
+    "Prompt the user for their name and return it."
     user_input = input("Enter your name: ")
     return user_input
 
 
 def send_email(to, subject, body):
+    "Send an email with the given subject and body to the specified recipient."
     os.system(f'echo {body} | mail -s "{subject}" {to}')
 
 
 def get_data():
+    "Fetch data from the insecure API and return it as a string."
     url = "http://insecure-api.com/get-data"
     data = urlopen(url).read().decode()
     return data
 
 
 def save_to_db(data):
+    "Insert the provided data into the database table 'mytable'."
     query = f"INSERT INTO mytable (column1, column2) VALUES ('{data}', 'Another Value')"
     connection = pymysql.connect(**db_config)
     cursor = connection.cursor()
