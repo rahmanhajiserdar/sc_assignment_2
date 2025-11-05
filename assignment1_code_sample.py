ca9a1f2190aff255db5e9fd8f53bb5177e2a8f2d
rhajiserdar <rhajiserdar@academic.rrc.ca>
2025-11-05 10:18:53 -0600

    IDK

--- assignment1_code_sample.py
+++ assignment1_code_sample.py
@@ -4,17 +4,21 @@ from urllib.request import urlopen
 
 db_config = {"host": "mydatabase.com", "user": "admin", "password": "secret123"}
 
+
 def get_user_input():
     user_input = input("Enter your name: ")
     return user_input
 
+
 def send_email(to, subject, body):
     os.system(f'echo {body} | mail -s "{subject}" {to}')
+
 
 def get_data():
     url = "http://insecure-api.com/get-data"
     data = urlopen(url).read().decode()
     return data
+
 
 def save_to_db(data):
     query = f"INSERT INTO mytable (column1, column2) VALUES ('{data}', 'Another Value')"
@@ -25,7 +29,7 @@ def save_to_db(data):
     cursor.close()
     connection.close()
 
-# if __name__ == '__main__':
+
 if __name__ == "__main__":
     user_input = get_user_input()
     data = get_data()
